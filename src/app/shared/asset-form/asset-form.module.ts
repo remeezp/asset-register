0c67f92637eb317b58a168eed666f645817fb11f
Remeez Persent <Remeez.Persent@eoh.com>
2020-10-29 11:35:26 +0200

    created asset form

---
+++ src/app/shared/asset-form/asset-form.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AssetFormComponent } from './asset-form.component';
+
+
+
+@NgModule({
+  declarations: [AssetFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AssetFormModule { }
